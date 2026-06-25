353b25a442d3369913688a77087572576de94cd1
Erick Xavier <erick.xavier@gmail.com>
2026-06-25 01:00:05 -0300

    chore: release v1.15.6

--- dist/esm/nojs-elements.js
+++ dist/esm/nojs-elements.js
@@ -1,5 +1,5 @@
 /**
- * NoJS Elements v1.15.5 — Element plugins for No.JS
+ * NoJS Elements v1.15.6 — Element plugins for No.JS
  * Drag-and-drop, and more.
  * @author exs.dev
  * @homepage https://elements.no-js.dev/
